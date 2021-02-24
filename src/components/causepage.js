9d2b3b6c4009fe5ec50ff25ca0dd1b2cd7f2d287
Luigi Agcaoili <luigiagcaoili@gmail.com>
2021-02-23 22:20:39 -0500

    Revert "Updates"
    This reverts commit b8220e88aae885d1ad01b966f7608320663d9c7c.

---
+++ src/components/causepage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "./Button";
+import PropTypes from "prop-types";
+import logo from "./images/logo.png";
+import gif from "./images/billnye.gif"
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      Climate change is devastating to communities globally.<br></br> Here's why
+      you should care
+  
+      <Link to = "/">
+       <img src={gif} />
+      </Link>
+
+      <Link to = "/causes">
+        <Button color="#2b2b2b" text="Causes " />
+      </Link>
+
+      <Link to = "/action">
+        <Button color="#2b2b2b" text="Action " />
+      </Link>
+
+      <Link to = "/donate">
+        <Button color="#2b2b2b" text="Donate " />
+      </Link>
+      
+      <Link to="/actnow">
+        <Button color="#2b2b2b" text="Act Now " />
+      </Link>
+   
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/act.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "../components/actnowpage";
+
+const actnow = () => {
+  return (
+    //root
+    <div className="container">
+      <Header />
+      <div className="content">
+        <h1>Climate change starts with you.</h1>
+        <h3>See how you can help.</h3>
+        <hr />
+        <h2 className="list-header">10 Impactful Steps</h2>
+        <ol className="list">
+          <li>Spread the word</li>
+          <li>Power your home with renewable energy</li>
+          <li>Invest in energy efficient appliances</li>
+          <li>Reduce water waste</li>
+          <li>Eat less meat</li>
+          <li>Save electricity</li>
+          <li>Drive a fuel-efficient vehicle</li>
+          <li>Maintain your ride</li>
+          <li>Avoid using cars</li>
+          <li>Shrink your carbon profile</li>
+        </ol>
+        <small className="note">
+          List courtesy of
+          https://www.nrdc.org/stories/how-you-can-stop-global-warming.
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default actnow;
